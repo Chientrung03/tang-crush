f8d6891e402bd11ac981ece77bbc70c1898dba99
Chientrung03 <86715560+Chientrung03@users.noreply.github.com>
2021-06-30 23:15:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     introTitle: 'Bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introDesc: `bạn à, tớ có điều muốn nói🙄🙄
+
+
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'bạn có thích tớ không đeyyyy😙',
+    desc: 'trả lời cho thật nhennn😗',
+    btnYes: 'có chứ, thích cậu lâu zoii<33',
+    btnNo: 'Không, bạn à :3',
+    question:'tại sao lại thích anh thế <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: ' tại anh dễ thương quá đấyy<33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
